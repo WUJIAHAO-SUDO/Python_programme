6a85199c2b4030d3454e712324a23a0ba3ace78e
WUJIAHAO-SUDO <1282405984@qq.com>
2025-05-12 21:08:01 +0800

    疫情采集信息项目.py

--- 疫情采集信息项目.py
+++ 疫情采集信息项目.py
@@ -27,6 +27,13 @@ class CoronaVirusSpider(object):
         #把json字符串转变为python格式
         data = json.loads(json_str)
         return data
+    
+    def load(self,path):
+        with open(path) as fp:
+            data = json.load(fp)
+        return data
+
+
     def save(self,data,savepath):
         #以json格式保存各国疫情数据
         with open(savepath,'w') as fp:
@@ -40,24 +47,23 @@ class CoronaVirusSpider(object):
         last_day_corona_virus = self.parse_home_page(home_page, tag_id='')
         self.save(last_day_corona_virus,'D:/竞赛和课程文件/编程语言/Python/基本编程语法学习笔记\爬虫/last_day_corona_virus.json')
 
-    def crawl_corona_virus(self,readpath,savepath):
+    def crawl_corona_virus(self,readpath,savepath,dec="采集1月23号以来各省疫情数据"):
         """采集从1月23号以来各国的疫情数据
         :return:"""
         #1.加载各国数据
-        with open(readpath) as fp:
-            last_day_corona_virus = json.loads(fp)
+        last_day_corona_virus = self.load(readpath)
         print(last_day_corona_virus)
         #定义列表，用于存储
         corona_virus = []
 
         #2.遍历各国数据，获取统计的url
-        for country in tqdm(last_day_corona_virus,"采集1月23号以来各国疫情数据"):
+        for country in tqdm(last_day_corona_virus,dec):
         #3.发送请求，获取json数据
             statistics_data_url = country["statisticsAata"]#找到这个键
             statistics_data_json_str = self.get_content_from_url(statistics_data_url)
             print(statistics_data_json_str)
         #4.把json转变为python，并存储到列表里
-            statistics_data = json.loads(statistics_data_json_str)
+            statistics_data = json.loads(statistics_data_json_str)['data']
             print(statistics_data)
         #5.把总数据列表以json格式保存为文件
             for oneday in statistics_data:
@@ -85,7 +91,29 @@ class CoronaVirusSpider(object):
         #3.保存疫情数据
         self.save(data,savepath)
 
+    #采集从1月23号以来我国各省的疫情数据
+    def crawl_corona_virus_of_china(self,readpath,savepath):
+        last_day_corona_virus_of_china = self.load(readpath)
+        corona_virus = self.new_method(last_day_corona_virus_of_china)
+        self.save(corona_virus,savepath)
 
+    def new_method(self, last_day_corona_virus_of_china,dec="采集1月23号以来各省疫情数据"):
+        corona_virus = []
+        #2.遍历各省数据，获取统计的url
+        for province in tqdm(last_day_corona_virus_of_china,dec):
+        #3.发送请求，获取各省json数据
+            statistics_data_url = province["statisticsAata"]#找到这个键
+            statistics_data_json_str = self.get_content_from_url(statistics_data_url)
+            print(statistics_data_json_str)
+        #4.把json转变为python，并存储到列表里
+            statistics_data = json.loads(statistics_data_json_str)['data']
+            print(statistics_data)
+        #5.把总数据列表以json格式保存为文件
+            for oneday in statistics_data:
+                oneday["provinceName"] = province["provinceName"]
+            print(statistics_data)
+            corona_virus.extend(statistics_data)
+        return corona_virus
 
     def run(self):
         self.crawl_last_day_corona_virus()
@@ -95,17 +123,3 @@ class CoronaVirusSpider(object):
 if __name__ == '__main__':
     spider = CoronaVirusSpider()
     spider.run()
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
